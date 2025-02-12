82eb4ce1cee9bf4549c12d7c8640d68eacac677e
silli <silli@10.0.0.232>
2025-02-11 21:46:42 -0500

    commit lab 1.6

---
+++ src/pack1/lab1.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("person details");
+		System.out.println("------");
+		System.out.println("first name: sill");
+		System.out.println("last name:vk ");
+		System.out.println("gender: F");
+
+	}
+
+}
